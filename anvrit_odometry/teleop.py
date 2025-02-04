2cd1ac390046003ac6744d89ef8ff1a1988a21e2
oGranny <ogranny.github.io@gmail.com>
2025-02-05 02:00:51 +0530

    added teleop support

---
+++ anvrit_odometry/teleop.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int32MultiArray
+from geometry_msgs.msg import Twist
+
+class TicksPublisher(Node):
+    def __init__(self):
+        super().__init__('circle_tracker')
+        self.publisher_ = self.create_publisher(Int32MultiArray, '/cmd_rpm', 10)
+        self.subscription = self.create_subscription(Twist, '/cmd_vel', self.listener_callback, 10)
+        self.SPEEDK = 10
+        self.RADIUS = 0.2
+
+    def listener_callback(self, msg):
+        rpm = Int32MultiArray()
+        linear_velocity = msg.linear.x
+        angular_velocity = msg.angular.z
+        self.get_logger().info(f"Received: {linear_velocity}, {angular_velocity}")
+
+        left_rpm = (linear_velocity - angular_velocity * self.RADIUS) * self.SPEEDK
+        right_rpm = (linear_velocity + angular_velocity * self.RADIUS) * self.SPEEDK
+        # print(left_rpm, right_rpm)
+        rpm.data = [min(int(abs(left_rpm)), 30), min(int(abs(right_rpm)), 30), self.sign(left_rpm), self.sign(right_rpm)]
+        self.get_logger().info(f"Publishing: {rpm.data}")
+        self.publisher_.publish(rpm)
+
+    def sign(self, a): return (a > 0) - (a < 0)
+
+def main(args=None):
+    rclpy.init(args=args)
+    ticks_pub = TicksPublisher()
+    rclpy.spin(ticks_pub)
+    ticks_pub.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
